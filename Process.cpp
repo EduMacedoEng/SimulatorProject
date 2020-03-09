58e24018a84d56e77996f3a4391023b556d65a74
Eduardo Macedo <eduardo.macedo@casablancaturismo.com.br>
2020-03-09 19:55:43 -0300

    process

--- Process.cpp
+++ Process.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 class Process {       // The class
   public:             // Access specifier
-    int process_id = 0;        // Attribute (int variable)
-    //srand(time(NULL));
-    int total_time = rand() % 100;
+    int process_id = 0; //  identificador do processo, inteiro sequencial começando em zero. 
+    int total_time = rand() % 20; //  tempo total de execução, aleatório dentro do intervalo (1, 20) segundos.
+    int state [3] = {1, 2, 3}; // 1 = Ready ; 2 = Running ; 3 = Terminated
+    int remaining_time = total_time; // Tempo de execução restante em segundos.
 };
