da5fa03e37295f17e70aa5094f5b8c32828ca7a1
Eduardo Macedo <eduardo.macedo@casablancaturismo.com.br>
2020-03-09 20:38:59 -0300

    process

--- Process.cpp
+++ Process.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 using namespace std;
 
+int range_time(int low, int high){
+      return rand() % (high - low) + low;
+    }
 class Process {       // The class
   public:             // Access specifier
     int process_id = 0; //  identificador do processo, inteiro sequencial começando em zero. 
-    int total_time = rand() % 20; //  tempo total de execução, aleatório dentro do intervalo (1, 20) segundos.
+    int total_time = range_time(1,20); //  tempo total de execução, aleatório dentro do intervalo (1, 20) segundos.
     int state [3] = {1, 2, 3}; // 1 = Ready ; 2 = Running ; 3 = Terminated
-    int remaining_time = total_time; // Tempo de execução restante em segundos.
+    int remaining_time = 0; // Tempo de execução restante em segundos.
 };
