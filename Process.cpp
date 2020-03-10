2f045195ef0fdd722d8a8c1ba7095e4c3a2ec37d
Eduardo Macedo <eduardo.macedo@casablancaturismo.com.br>
2020-03-10 10:20:09 -0300

    process

--- Process.cpp
+++ Process.cpp
@@ -7,6 +7,6 @@ class Process {       // The class
   public:             // Access specifier
     int process_id = 0; //  identificador do processo, inteiro sequencial começando em zero. 
     int total_time; //  tempo total de execução, aleatório dentro do intervalo (1, 20) segundos.
-    string state [3] = {"Ready", "Running", "Terminated"}; // 1 = Ready ; 2 = Running ; 3 = Terminated
+    string state [3] = {"Ready", "Running", "Terminated"}; // 0 = Ready ; 1 = Running ; 2 = Terminated
     int remaining_time; // Tempo de execução restante em segundos.
 };
