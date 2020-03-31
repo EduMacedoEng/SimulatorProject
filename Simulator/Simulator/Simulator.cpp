5dd727d833f362cae5e4791c26ad52530fd96b15
LeonardoYan <61258300+LeonardoYan@users.noreply.github.com>
2020-03-31 17:39:17 -0300

    Add files via upload

--- Simulator/Simulator/Simulator.cpp
+++ Simulator/Simulator/Simulator.cpp
@@ -1,112 +1,65 @@
 #include "Simulator.h"
-#include "Scheduler.h"
 #include "Kernel.h"
-#include "Process.h"
 #include "core.h"
 
-const int N = 4;
-const int M = 100;
+//const int N = 4; // Constante que representa o numero de atributos de cada processo
+//const int M = 100;
+
+Kernel* kernel = new Kernel();
+
+
 Simulator::Simulator() {
 	//
 }
 
-//Retorna o inicio do processo
-void start() {
-	//Start o simulador
-	cout << "Simulator started" << endl;
-   
-}
-
 // Função que gera randomicamente números em um intervalo de tempo
 int randRange(int low, int high) {
-
 	return rand() % (high - low) + low;
-
 };
-
-void printMatriz(int** matriz, int qtd_process) {
-	for (int i = 0; i < qtd_process; i++)
-	{
-		cout << matriz[i][0] << "\t\t\t"
-			<< matriz[i][1] << "\t\t\t"
-			<< matriz[i][2] << "\t\t\t"
-			<< matriz[i][3] << endl;
-	}
-}
-
-void printArray(int** arr, int qtd_process) {
-	for (int i = 0; i < qtd_process; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << arr[i][j] << "|";
-		}
-		cout << endl;
-	}
-}
 
 void Simulator::run(int qtd_process) {
 
-	Kernel* kernel = new Kernel();
-
 	cout << "Simulator started" << endl;
-	int** matriz = batch_process_init(qtd_process);
-	cout << "__________________" << endl;
-	int** matriz_kernel = kernel->create_process(matriz,qtd_process);
-	printArray(matriz_kernel, qtd_process);
+	//int** matriz = batch_process_init();
+	batch_process_init(qtd_process);
+	//int** matriz_kernel = kernel->create_process(matriz);
+	//printMatrizProcess(matriz_kernel, qtd_process);
 }
 
-
-int** Simulator::batch_process_init(int qtd_process) {
-	int** matriz = create_random_process(qtd_process);
-	return matriz;
-}
-
-int** getArray(int qtd_process) {
-	int** arr = new int* [qtd_process];
-	for (int i = 0; i < qtd_process; i++) {
-		arr[i] = new int[qtd_process];
-		for (int j = 0; j < N; j++) {
-			arr[i][0] = (i + j - 2);
-			arr[i][1] = randRange(1, 20);
-			arr[i][2] = 0;
-			arr[i][3] = arr[i][1];
-		}
-	}
-	return arr;
-}
-
-int** Simulator::create_random_process(int qtd_process) {
+void Simulator::batch_process_init(int qtd_process) {
 	
-	Process* arrayProcess = new Process[qtd_process];
-	int state_id = 0;
-	//int** matriz = new int*[100];
-	int** matriz = getArray(qtd_process);
-
+	kernel->Process_Control_Table = new Process[qtd_process];
+	int status = 0;
 	for (int i = 0; i < qtd_process; i++) {
-		for (int j = 0; i < 4; i++)
-		{
-			//matriz[i][0] = (i+j-2);
-			matriz[i][1] = randRange(1, 20);
-			matriz[i][2] = state_id;
-			matriz[i][3] = matriz[i][1];
-		}
-
+		int state_id = 0;
+		kernel->Process_Control_Table[i].state[state_id];
+		kernel->Process_Control_Table[i].total_time = randRange(1, 20);
+		kernel->Process_Control_Table[i].quantum = randRange(1, 10);
+		kernel->Process_Control_Table[i].remaining_time = kernel->Process_Control_Table[i].total_time;
+		kernel->Process_Control_Table[i].process_id = i;
+		kernel->Process_Control_Table[i].showProcess(state_id);
 	}
-	printArray(matriz, qtd_process);
-	return matriz;
 }
 
-
-
+void Simulator::create_random_process() {
+	kernel->create_process();
+}
 
 int main()
 {
 	//inicio main
+	
 	Simulator s;
 	int qtd_process;
+
 	cout << "Quantidade de processos : ";
 	cin >> qtd_process;
-	//s.create_random_process(qtd_process);
 	s.run(qtd_process);
+
+
+	
+
+
 
 	//final main
 	_getch(); //pause final
--- Simulator/Simulator/Simulator.h
+++ Simulator/Simulator/Simulator.h
@@ -2,7 +2,6 @@
 #include <cstdlib>
 #include <conio.h>
 #include <windows.h>
-#include "Kernel.h"
 #include <time.h>
 #include <stdio.h>
 #include <chrono>
@@ -16,10 +15,13 @@ public:
     Simulator();
 
 
-    int** create_random_process(int qtd_process);
-    int** batch_process_init(int qtd_process);
+    //int** create_random_process(int qtd_process);
+    void create_random_process();
+    //int** batch_process_init(int qtd_process);
+    void batch_process_init(int qtd_processos);
+    //void printMatrizProcess(int** arr, int qtd_process);
+    //int** delete_matriz(int** matriz, int i, int j, int qtd_processos);
     void run(int qtd_process);
-
 
     int processor_cores_number;
     int quantum;
